b2ead363cc5f875f8acaf5fc9deff1ec79cb45b8
Enoch Taylor <tay121222@gmail.com>
2023-09-25 11:09:49 +0000

    last task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,14 @@
 # Puppet manifest to configure SSH client
-file { '/etc/ssh/ssh_config':
-  ensure  => 'file',
-  content => "IdentityFile ~/.ssh/school\nPasswordAuthentication no\n",
-  replace => 'no',
-  owner   => 'tay121222',
-  mode    => '0600',
+exec { 'ssh_identityfile':
+  command    => "grep -q '^IdentityFile ~/.ssh/school' ~/.ssh/config || echo 'IdentityFile ~/.ssh/school' >> ~/.ssh/config",
+  path    => '/bin:/usr/bin:/usr/local/bin',
+  creates     => '/home/tay121222/.ssh/config',
+  refreshonly => true,
+}
+
+exec { 'ssh_passwordauthentication':
+  command => "grep -q '^PasswordAuthentication no' ~/.ssh/config || echo 'PasswordAuthentication no' >> ~/.ssh/config"
+  path    => '/bin:/usr/bin:/usr/local/bin',
+  creates     => '/home/tay121222/.ssh/config',
+  refreshonly => true,
 }

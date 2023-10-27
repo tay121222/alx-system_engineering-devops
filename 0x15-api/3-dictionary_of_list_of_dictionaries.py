6879b84170ae26021d544098102d5c9ea62d030e
Enoch Taylor <tay121222@gmail.com>
2023-10-27 09:55:53 +0000

    Dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Retrieves information about employee TODO list progress from a REST API.
+
+Usage:
+python3 filename.py <employee_id>
+"""
+import requests
+import sys
+import json
+
+base_url = 'https://jsonplaceholder.typicode.com'
+
+
+def get_todo():
+    """Fetch to task done by employee"""
+    all_todos = {}
+    users = requests.get('{}/users'.format(base_url)).json()
+
+    for user in users:
+        user_id = user['id']
+        todos = requests.get('{}/todos?userId={}'.format(
+            base_url, user_id)
+            ).json()
+        all_todos[user_id] = []
+        for todo in todos:
+            all_todos[user_id].append({
+                "username": user['username'],
+                "task": todo['title'],
+                "completed": todo['completed']
+            })
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(all_todos, f)
+
+
+if __name__ == "__main__":
+    get_todo()

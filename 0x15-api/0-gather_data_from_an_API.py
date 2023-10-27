439f6f925d8b3b0e386ef376e7d94f3c5807f26b
Enoch Taylor <tay121222@gmail.com>
2023-10-27 10:11:14 +0000

    Trying to fix structure issues

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,26 +9,30 @@ import requests
 import sys
 
 
-def get_todo(id):
+def gettodo(id):
     """Fetch to task done by employee"""
     employee_id = id
-    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
     response = requests.get(url)
     employee = response.json()
-    employee_name = employee['name']
+    employee_name = employee["name"]
 
-    url = 'https://jsonplaceholder.typicode.com/todos?'
-    param = 'userId={}'.format(employee_id)
+    url = "https://jsonplaceholder.typicode.com/todos?"
+    param = "userId={}".format(employee_id)
     response = requests.get(url + param)
     todos = response.json()
-    completed_tasks = [todo['title'] for todo in todos if todo['completed']]
+    completed_tasks = [todo["title"] for todo in todos if todo["completed"]]
     total_tasks = len(todos)
 
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employee_name, len(completed_tasks), total_tasks))
+    print(
+        "Employee {} is done with tasks({}/{}):".format(
+            employee_name, len(completed_tasks), total_tasks
+        )
+    )
+
     for task in completed_tasks:
         print("\t{}".format(task))
 
 
 if __name__ == "__main__":
-    get_todo(int(sys.argv[1]))
+    gettodo(int(sys.argv[1]))

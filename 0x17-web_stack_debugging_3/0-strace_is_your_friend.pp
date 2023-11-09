f5170cf66d4acee6a8ec462623f955caa0c63a3b
Enoch Taylor <tay121222@gmail.com>
2023-11-09 13:48:55 +0000

    fix why Apache is returning a 500 error - updated 3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,5 +3,4 @@ $file_path = '/var/www/html/wp-settings.php'
 exec { 'fix_wordpress':
   command     => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' ${file_path}",
   path        => ['/bin', '/usr/bin'],
-  refreshonly => true,
 }

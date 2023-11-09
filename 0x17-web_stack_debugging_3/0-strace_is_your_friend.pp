b5dc2091eefb79226009402fbc3f3f23e8d2ad04
Enoch Taylor <tay121222@gmail.com>
2023-11-09 13:09:34 +0000

    fix why Apache is returning a 500 error - updated

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 # Script that fix why Apache is returning a 500 error
-file { '/var/www/html/wp-includes/wp-settings.php':
+file { '/var/www/html/wp-settings.php':
   ensure  => file,
-  require => File['/var/www/html/wp-includes'],
+  require => File['/var/www/html'],
   notify  => Service['apache2'],
   before  => Exec['fix-wordpress'],
 }
@@ -9,6 +9,6 @@ file { '/var/www/html/wp-includes/wp-settings.php':
 exec { 'fix-wordpress':
   command     => 'sed -i "s/require( ABSPATH . WPINC . \'\/class-wp-locale.phpp\' );/' \
   'require_once( ABSPATH . WPINC . \'\/class-wp-locale.php\' );/g" ' \
-  '/var/www/html/wp-includes/wp-settings.php',
+  '/var/www/html/wp-settings.php',
   refreshonly => true,
 }
